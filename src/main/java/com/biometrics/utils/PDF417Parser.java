3652ec6a686055ac00cd19b6ba000360e2179cbb
luis.amengual <luis.amengual@globant.com>
2020-05-21 13:28:44 -0300

    Separacion de código para parseo de MRZ y PDF417

--- src/main/java/com/biometrics/utils/PDF417Parser.java
+++ src/main/java/com/biometrics/utils/PDF417Parser.java
@@ -10,11 +10,19 @@ public class PDF417Parser {
         Map<String, Object> documentData = new HashMap<>();
         documentData.put(Document.FIRST_NAME_FIELD, formatName(dataTokens[2]));
         documentData.put(Document.LAST_NAME_FIELD, formatName(dataTokens[1]));
-        documentData.put(Document.DOCUMENT_NUMBER_FIELD, dataTokens[4]);
+        documentData.put(Document.DOCUMENT_NUMBER_FIELD, formatDocumentNumber(dataTokens[4]));
         documentData.put(Document.GENDER_PROPERTY_FIELD, dataTokens[3]);
         documentData.put(Document.BIRTH_DATE_FIELD, formatDate(dataTokens[6]));
-        documentData.put(Document.NATIONAL_IDENTFICATION_NUMBER_FIELD, dataTokens[0]);
+        documentData.put(Document.NATIONAL_IDENTFICATION_NUMBER_FIELD, formatNationalIdentificationNumber(dataTokens[0]));
         return documentData;
+    }
+
+    private static int formatDocumentNumber(final String text) {
+        return Integer.parseInt(text.replaceAll("[^\\d.]", ""));
+    }
+
+    private static int formatNationalIdentificationNumber(final String text) {
+        return Integer.parseInt(text.replaceAll("[^\\d.]", ""));
     }
 
     private static String formatName(final String text) {
