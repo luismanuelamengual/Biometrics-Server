1857117c897ae950b8088548b3572961a7880e2f
luis.amengual <luis.amengual@globant.com>
2020-07-06 16:30:59 -0300

    Corrección menor

--- src/main/java/com/biometrics/api/v1/ApiController.java
+++ src/main/java/com/biometrics/api/v1/ApiController.java
@@ -5,6 +5,10 @@ import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
 import com.amazonaws.services.rekognition.model.CompareFacesMatch;
 import com.amazonaws.services.rekognition.model.CompareFacesRequest;
 import com.amazonaws.services.rekognition.model.CompareFacesResult;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.biometrics.utils.MRZParser;
 import com.biometrics.utils.OpenCVUtils;
 import com.biometrics.utils.PDF417Parser;
@@ -13,6 +17,8 @@ import com.dynamsoft.barcode.TextResult;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.util.LoadLibs;
 import org.neogroup.warp.controllers.ControllerComponent;
+import org.neogroup.warp.controllers.routing.Body;
+import org.neogroup.warp.controllers.routing.HeaderParameter;
 import org.neogroup.warp.controllers.routing.Parameter;
 import org.neogroup.warp.controllers.routing.Post;
 import org.neogroup.warp.data.Data;
@@ -25,12 +31,22 @@ import org.opencv.objdetect.CascadeClassifier;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static org.neogroup.warp.Warp.getProperty;
 
 @ControllerComponent("v1")
 public class ApiController {
+
+    private static final String LIVENESS_SESSION_SECRET_KEY_PROPERTY_NAME = "liveness_session_secret_key";
+    private static final int LIVENESS_STATUS_RUNNING = 0;
+    private static final int LIVENESS_STATUS_COMPLETED = 1;
+    private static final int LIVENESS_STATUS_TIMEOUT = 2;
+    private static final int LIVENESS_STATUS_ERROR = 2;
+    private static final String LIVENESS_INSTRUCTIONS_CLAIM_NAME = "instructions";
+    private static final String LIVENESS_TIMEOUT_TIMESTAMP_CLAIM_NAME = "timeoutTimestamp";
+    private static final String LIVENESS_INSTRUCTION_CLAIM_NAME = "instructionIndex";
 
     private static final int FACE_MATCH_SUCCESS_STATUS_CODE = 0;
     private static final int FACE_WITH_INCORRECT_GESTURE_STATUS_CODE = 1;
@@ -45,6 +61,7 @@ public class ApiController {
     private static final String FRONTAL_FACE_INSTRUCTION = "frontal_face";
     private static final String LEFT_PROFILE_FACE_INSTRUCTION = "left_profile_face";
     private static final String RIGHT_PROFILE_FACE_INSTRUCTION = "right_profile_face";
+    private static final String[] ALL_INSTRUCTIONS = { FRONTAL_FACE_INSTRUCTION, LEFT_PROFILE_FACE_INSTRUCTION, RIGHT_PROFILE_FACE_INSTRUCTION };
 
     private static final String MATCH_PROPERTY_NAME = "match";
     private static final String STATUS_PROPERTY_NAME = "status";
@@ -52,6 +69,9 @@ public class ApiController {
     private static final String TYPE_PROPERTY_NAME = "type";
     private static final String RAW_PROPERTY_NAME = "raw";
     private static final String INFORMATION_PROPERTY_NAME = "information";
+    private static final String SESSION_PROPERTY_NAME = "session";
+    private static final String INSTRUCTION_PROPERTY_NAME = "instruction";
+    private static final String MESSAGE_PROPERTY_NAME = "message";
 
     private CascadeClassifier faceClassfier;
     private CascadeClassifier profileFaceClassifier;
@@ -59,8 +79,12 @@ public class ApiController {
     private CascadeClassifier eyePairClassifier;
     private Tesseract tesseract;
     private AmazonRekognition rekognitionClient;
+    private Algorithm livenessJwtAlgorithm;
+    private JWTVerifier livenessJwtVerifier;
 
     public ApiController() {
+        livenessJwtAlgorithm = Algorithm.HMAC256(getProperty(LIVENESS_SESSION_SECRET_KEY_PROPERTY_NAME));
+        livenessJwtVerifier = JWT.require(livenessJwtAlgorithm).build();
         faceClassfier = OpenCVUtils.getClassfierFromResource("cascades/face.xml");
         profileFaceClassifier = OpenCVUtils.getClassfierFromResource("cascades/profile-face.xml");
         eyeClassifier = OpenCVUtils.getClassfierFromResource("cascades/eye.xml");
@@ -71,13 +95,182 @@ public class ApiController {
         tesseract.setLanguage("spa");
     }
 
-    @Post("check_liveness_instruction")
-    public DataObject checkLivenessInstruction(@Parameter("instruction") String instruction, @Parameter("selfie") byte[] selfie) throws Exception {
+    @Post("start_liveness_session")
+    public DataObject startLivenessSession(@Parameter(value="maxInstructions", required=false) int maxInstructions, @Parameter(value="sessionTimeout", required=false) long sessionTimeout) {
 
+        if (maxInstructions < 2) {
+            maxInstructions = 3;
+        }
+        if (sessionTimeout == 0) {
+            sessionTimeout = 60000L;
+        }
+
+        Random rand = new Random();
+        Integer[] instructions = new Integer[maxInstructions];
+        int currentInstructionIndex = Arrays.binarySearch(ALL_INSTRUCTIONS, FRONTAL_FACE_INSTRUCTION);
+        instructions[0] = currentInstructionIndex;
+        for (int instructionSlot = 1; instructionSlot < maxInstructions; instructionSlot++) {
+            List<Integer> possibleInstructions = new ArrayList<>();
+            for (int instructionIndex = 0; instructionIndex < ALL_INSTRUCTIONS.length; instructionIndex++) {
+                if (instructionIndex != currentInstructionIndex) {
+                    possibleInstructions.add(instructionIndex);
+                }
+            }
+            int nextInstructionIndex = rand.nextInt(possibleInstructions.size());
+            currentInstructionIndex = possibleInstructions.get(nextInstructionIndex);
+            instructions[instructionSlot] = currentInstructionIndex;
+        }
+
+        long timeoutTimestamp = System.currentTimeMillis() + sessionTimeout;
+        String session = createLivenessSession(instructions, timeoutTimestamp, 0);
+        return Data.object()
+            .set(SESSION_PROPERTY_NAME, session)
+            .set(STATUS_PROPERTY_NAME, LIVENESS_STATUS_RUNNING)
+            .set(INSTRUCTION_PROPERTY_NAME, ALL_INSTRUCTIONS[instructions[0]]);
+    }
+
+    @Post("post_liveness_image")
+    public DataObject postLivenessImage(@HeaderParameter("Liveness-Session-Id") String session, @Body byte[] image) {
+        DataObject response = Data.object();
+        try {
+            DecodedJWT decodedJWT = livenessJwtVerifier.verify(session);
+            long expirationTimestamp = decodedJWT.getClaim(LIVENESS_TIMEOUT_TIMESTAMP_CLAIM_NAME).asLong();
+            if (System.currentTimeMillis() > expirationTimestamp) {
+                response.set(SESSION_PROPERTY_NAME, session);
+                response.set(STATUS_PROPERTY_NAME, LIVENESS_STATUS_TIMEOUT);
+            } else {
+                Integer[] instructions = decodedJWT.getClaim(LIVENESS_INSTRUCTIONS_CLAIM_NAME).asArray(Integer.class);
+                int instructionIndex = decodedJWT.getClaim(LIVENESS_INSTRUCTION_CLAIM_NAME).asInt();
+                int lastInstructionIndex = instructionIndex;
+                String instruction = ALL_INSTRUCTIONS[instructions[instructionIndex]];
+                int status = checkLivenessImage(instruction, image);
+                int livenessStatus = LIVENESS_STATUS_RUNNING;
+                switch (status) {
+                    case FACE_NOT_FOUND_STATUS_CODE:
+                    case FACE_NOT_CENTERED_STATUS_CODE:
+                    case FACE_TOO_CLOSE_STATUS_CODE:
+                    case FACE_TOO_FAR_AWAY_STATUS_CODE:
+                        instructionIndex = 0;
+                        break;
+                    case FACE_WITH_INCORRECT_GESTURE_STATUS_CODE:
+                        break;
+                    case FACE_MATCH_SUCCESS_STATUS_CODE:
+                        if (instructionIndex >= instruction.length() - 1) {
+                            livenessStatus = LIVENESS_STATUS_COMPLETED;
+                        } else {
+                            instructionIndex++;
+                        }
+                        break;
+                }
+                if (instructionIndex != lastInstructionIndex) {
+                    session = createLivenessSession(instructions, expirationTimestamp, instructionIndex);
+                }
+                response.set(SESSION_PROPERTY_NAME, session);
+                response.set(STATUS_PROPERTY_NAME, livenessStatus);
+                response.set(INSTRUCTION_PROPERTY_NAME, ALL_INSTRUCTIONS[instructions[instructionIndex]]);
+            }
+
+        } catch (Exception e) {
+            response.set(SESSION_PROPERTY_NAME, session);
+            response.set(STATUS_PROPERTY_NAME, LIVENESS_STATUS_ERROR);
+            response.set(MESSAGE_PROPERTY_NAME, e.getMessage());
+        }
+        return response;
+    }
+
+    public String createLivenessSession(Integer[] instructions, long timeoutTimestamp, int instructionIndex) {
+        return JWT.create()
+            .withArrayClaim(LIVENESS_INSTRUCTIONS_CLAIM_NAME, instructions)
+            .withClaim(LIVENESS_TIMEOUT_TIMESTAMP_CLAIM_NAME, timeoutTimestamp)
+            .withClaim(LIVENESS_INSTRUCTION_CLAIM_NAME, instructionIndex)
+            .sign(livenessJwtAlgorithm);
+    }
+
+    @Post("check_liveness_instruction")
+    @Deprecated
+    public DataObject checkLivenessInstruction(@Parameter("instruction") String instruction, @Parameter("selfie") byte[] selfie) throws Exception {
         if (!instruction.equals(FRONTAL_FACE_INSTRUCTION) && !instruction.equals(LEFT_PROFILE_FACE_INSTRUCTION) && !instruction.equals(RIGHT_PROFILE_FACE_INSTRUCTION)) {
             throw new Exception("Unrecognized liveness instruction");
         }
+        int status = checkLivenessImage(instruction, selfie);
+        return Data.object()
+            .set(MATCH_PROPERTY_NAME, status == FACE_MATCH_SUCCESS_STATUS_CODE)
+            .set(STATUS_PROPERTY_NAME, status);
+    }
 
+    @Post("verify_identity")
+    public DataObject verifyIdentity(@Parameter("selfie") byte[] selfie, @Parameter("documentFront") byte[] documentFront, @Parameter("documentBack") byte[] documentBack) {
+        Mat selfieMat = OpenCVUtils.detectBiggestFeature(OpenCVUtils.getMat(selfie), faceClassfier);
+        if (selfieMat == null || selfieMat.empty()) {
+            throw new RuntimeException ("No face found in selfie");
+        }
+        Mat documentFaceMat = OpenCVUtils.detectBiggestFeature(OpenCVUtils.getMat(documentFront), faceClassfier);
+        if (documentFaceMat == null || documentFaceMat.empty()) {
+            documentFaceMat = OpenCVUtils.detectBiggestFeature(OpenCVUtils.getMat(documentBack), faceClassfier);
+        }
+        if (documentFaceMat == null || documentFaceMat.empty()) {
+            throw new RuntimeException ("No face found in document");
+        }
+
+        byte[] selfieBytes = OpenCVUtils.getImageBytes(selfieMat);
+        byte[] documentSelfieBytes = OpenCVUtils.getImageBytes(documentFaceMat);
+        boolean match = false;
+        float similarity = compareFacesInImages (selfieBytes, documentSelfieBytes);
+        if (similarity > 0) {
+            match = true;
+        }
+        return Data.object()
+            .set(MATCH_PROPERTY_NAME, match)
+            .set(SIMILARITY_PROPERTY_NAME, similarity);
+    }
+
+    @Post("scan_document_data")
+    public DataObject scanDocument(@Parameter("documentFront") byte[] documentFront, @Parameter("documentBack") byte[] documentBack) throws Exception {
+
+        DataObject response = null;
+        String pdf417RawText = getPDF417CodeFromImage(documentFront);
+        if (pdf417RawText == null) {
+            pdf417RawText = getPDF417CodeFromImage(documentBack);
+        }
+
+        if (pdf417RawText != null) {
+            try {
+                Map<String, Object> documentInformation = PDF417Parser.parseCode(pdf417RawText);
+                if (documentInformation != null && !documentInformation.isEmpty()){
+                    response = Data.object()
+                            .set(TYPE_PROPERTY_NAME, PDF417_TYPE)
+                            .set(RAW_PROPERTY_NAME, pdf417RawText)
+                            .set(INFORMATION_PROPERTY_NAME, documentInformation);
+                }
+            } catch (Exception ex) {}
+        }
+
+        if (response == null) {
+            String mrzRawText = getMRZCodeFromImage(documentBack);
+            if (mrzRawText == null) {
+                mrzRawText = getMRZCodeFromImage(documentFront);
+            }
+
+            if (mrzRawText != null) {
+                try {
+                    Map<String, Object> documentInformation = MRZParser.parseCode(mrzRawText);
+                    if (documentInformation != null && !documentInformation.isEmpty()){
+                        response = Data.object()
+                                .set(TYPE_PROPERTY_NAME, MRZ_TYPE)
+                                .set(RAW_PROPERTY_NAME, mrzRawText)
+                                .set(INFORMATION_PROPERTY_NAME, documentInformation);
+                    }
+                } catch(Exception ex) {}
+            }
+        }
+
+        if (response == null) {
+            throw new RuntimeException("Document data could not be read");
+        }
+        return response;
+    }
+
+    private int checkLivenessImage(String instruction, byte[] selfie) {
         Mat image = OpenCVUtils.getMat(selfie);
         Rect frontalFaceRect = OpenCVUtils.detectBiggestFeatureRect(image, faceClassfier);
         Rect eyePairRect = OpenCVUtils.detectBiggestFeatureRect(image, eyePairClassifier);
@@ -169,82 +362,7 @@ public class ApiController {
                 }
             }
         }
-
-        return Data.object()
-            .set(MATCH_PROPERTY_NAME, status == FACE_MATCH_SUCCESS_STATUS_CODE)
-            .set(STATUS_PROPERTY_NAME, status);
-    }
-
-    @Post("verify_identity")
-    public DataObject verifyIdentity(@Parameter("selfie") byte[] selfie, @Parameter("documentFront") byte[] documentFront, @Parameter("documentBack") byte[] documentBack) {
-        Mat selfieMat = OpenCVUtils.detectBiggestFeature(OpenCVUtils.getMat(selfie), faceClassfier);
-        if (selfieMat == null || selfieMat.empty()) {
-            throw new RuntimeException ("No face found in selfie");
-        }
-        Mat documentFaceMat = OpenCVUtils.detectBiggestFeature(OpenCVUtils.getMat(documentFront), faceClassfier);
-        if (documentFaceMat == null || documentFaceMat.empty()) {
-            documentFaceMat = OpenCVUtils.detectBiggestFeature(OpenCVUtils.getMat(documentBack), faceClassfier);
-        }
-        if (documentFaceMat == null || documentFaceMat.empty()) {
-            throw new RuntimeException ("No face found in document");
-        }
-
-        byte[] selfieBytes = OpenCVUtils.getImageBytes(selfieMat);
-        byte[] documentSelfieBytes = OpenCVUtils.getImageBytes(documentFaceMat);
-        boolean match = false;
-        float similarity = compareFacesInImages (selfieBytes, documentSelfieBytes);
-        if (similarity > 0) {
-            match = true;
-        }
-        return Data.object()
-            .set(MATCH_PROPERTY_NAME, match)
-            .set(SIMILARITY_PROPERTY_NAME, similarity);
-    }
-
-    @Post("scan_document_data")
-    public DataObject scanDocument(@Parameter("documentFront") byte[] documentFront, @Parameter("documentBack") byte[] documentBack) throws Exception {
-
-        DataObject response = null;
-        String pdf417RawText = getPDF417CodeFromImage(documentFront);
-        if (pdf417RawText == null) {
-            pdf417RawText = getPDF417CodeFromImage(documentBack);
-        }
-
-        if (pdf417RawText != null) {
-            try {
-                Map<String, Object> documentInformation = PDF417Parser.parseCode(pdf417RawText);
-                if (documentInformation != null && !documentInformation.isEmpty()){
-                    response = Data.object()
-                            .set(TYPE_PROPERTY_NAME, PDF417_TYPE)
-                            .set(RAW_PROPERTY_NAME, pdf417RawText)
-                            .set(INFORMATION_PROPERTY_NAME, documentInformation);
-                }
-            } catch (Exception ex) {}
-        }
-
-        if (response == null) {
-            String mrzRawText = getMRZCodeFromImage(documentBack);
-            if (mrzRawText == null) {
-                mrzRawText = getMRZCodeFromImage(documentFront);
-            }
-
-            if (mrzRawText != null) {
-                try {
-                    Map<String, Object> documentInformation = MRZParser.parseCode(mrzRawText);
-                    if (documentInformation != null && !documentInformation.isEmpty()){
-                        response = Data.object()
-                                .set(TYPE_PROPERTY_NAME, MRZ_TYPE)
-                                .set(RAW_PROPERTY_NAME, mrzRawText)
-                                .set(INFORMATION_PROPERTY_NAME, documentInformation);
-                    }
-                } catch(Exception ex) {}
-            }
-        }
-
-        if (response == null) {
-            throw new RuntimeException("Document data could not be read");
-        }
-        return response;
+        return status;
     }
 
     private String getMRZCodeFromImage (byte[] imageBytes) throws Exception {
@@ -286,8 +404,6 @@ public class ApiController {
         }
         return pdf417Code;
     }
-
-
 
     private float compareFacesInImages (byte[] image1Bytes, byte[] image2Bytes) {
         com.amazonaws.services.rekognition.model.Image image1 = new com.amazonaws.services.rekognition.model.Image().withBytes(ByteBuffer.wrap(image1Bytes));;
