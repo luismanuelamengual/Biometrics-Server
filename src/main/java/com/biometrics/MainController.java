2252a438b2070cddf2d186f4450de9be60135a3d
luis.amengual <luis.amengual@globant.com>
2020-11-01 10:54:37 -0300

    Implementación de registros de log mejorados

--- src/main/java/com/biometrics/MainController.java
+++ src/main/java/com/biometrics/MainController.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.neogroup.warp.Request;
 import org.neogroup.warp.Response;
 import org.neogroup.warp.controllers.ControllerComponent;
@@ -12,9 +13,6 @@ import org.neogroup.warp.controllers.routing.Error;
 import org.neogroup.warp.controllers.routing.*;
 import org.neogroup.warp.data.Data;
 import org.neogroup.warp.data.DataObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.neogroup.warp.Warp.getLogger;
 import static org.neogroup.warp.Warp.getProperty;
@@ -25,26 +23,26 @@ public class MainController {
     private static final String BIOMETRICS_JWT_SECRET_KEY_PROPERTY_NAME = "api_key_secret_key";
     private static final String AUTHORIZATION_HEADER_NAME = "Authorization";
     private static final String AUTHORIZATION_BEARER = "Bearer";
-    private static final String API_KEY_PARAMETER_NAME = "apiKey";
+    private static final String CLIENT_PARAMETER_NAME = "client";
     private static final String SUCCESS_PARAMETER_NAME = "success";
     private static final String DATA_PARAMETER_NAME = "data";
     private static final String MESSAGE_PARAMETER_NAME = "message";
+    private static final String RESPONSE_PARAMETER_NAME = "response";
+    private static final String METHOD_PARAMETER_NAME = "method";
+    private static final String PATH_PARAMETER_NAME = "path";
 
     private JWTVerifier jwtVerifier;
+    private Algorithm jwtSigningAlgorithm;
     private JsonFormatter jsonFormatter;
 
     public MainController() {
-        String secretKey = getProperty(BIOMETRICS_JWT_SECRET_KEY_PROPERTY_NAME);
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        jwtVerifier = JWT.require(algorithm).withIssuer("auth0").build();
+        jwtSigningAlgorithm = Algorithm.HMAC256(getProperty(BIOMETRICS_JWT_SECRET_KEY_PROPERTY_NAME));
+        jwtVerifier = JWT.require(jwtSigningAlgorithm).withIssuer("auth0").build();
         jsonFormatter = new JsonFormatter();
     }
 
-    @Get("session")
-    public String createSession(@Parameter("client") String clientName) throws Exception {
-        String secretKey = getProperty(BIOMETRICS_JWT_SECRET_KEY_PROPERTY_NAME);
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        return JWT.create().withIssuer("auth0").withClaim("client", clientName).sign(algorithm);
+    public String createSession(String clientName) throws Exception {
+        return JWT.create().withIssuer("auth0").withClaim(CLIENT_PARAMETER_NAME, clientName).sign(jwtSigningAlgorithm);
     }
 
     @Before("*")
@@ -65,8 +63,8 @@ public class MainController {
         }
         String token = authorizationTokens[1];
         try {
-            jwtVerifier.verify(token);
-            request.set(API_KEY_PARAMETER_NAME, token);
+            DecodedJWT verifiedToken = jwtVerifier.verify(token);
+            request.set(CLIENT_PARAMETER_NAME, verifiedToken.getClaim(CLIENT_PARAMETER_NAME).asString());
         } catch (JWTVerificationException verificationException) {
             response.setStatus(401);
             throw new ResponseException("Invalid authentication token");
@@ -84,7 +82,7 @@ public class MainController {
         DataObject result = Data.object();
         result.set(SUCCESS_PARAMETER_NAME, false);
         result.set(MESSAGE_PARAMETER_NAME, exception.getMessage());
-        getLogger().info(jsonFormatter.format(result));
+        getLogger().info(jsonFormatter.format(getLogData(request, result)));
         return result;
     }
 
@@ -96,22 +94,18 @@ public class MainController {
         if (responseObject != null) {
             result.set(DATA_PARAMETER_NAME, responseObject);
         }
-        getLogger().info(jsonFormatter.format(result));
+        getLogger().info(jsonFormatter.format(getLogData(request, result)));
         return result;
     }
 
-    private Object getLogData(Object object) {
-        Object logData = null;
-        if (object instanceof DataObject) {
-            Map<String, Object> properties = new HashMap<>();
-            DataObject dataObject = (DataObject)object;
-            for (String property : dataObject.properties()) {
-                properties.put(property, getLogData(dataObject.get(property)));
-            }
-            logData = properties;
-        } else {
-            logData = object;
+    public DataObject getLogData(Request request, Object response) {
+        DataObject data = Data.object();
+        data.set(METHOD_PARAMETER_NAME, request.getMethod());
+        data.set(PATH_PARAMETER_NAME, request.getRequestURI());
+        if (request.has(CLIENT_PARAMETER_NAME)) {
+            data.set(CLIENT_PARAMETER_NAME, request.get(CLIENT_PARAMETER_NAME));
         }
-        return logData;
+        data.set(RESPONSE_PARAMETER_NAME, response);
+        return data;
     }
 }
