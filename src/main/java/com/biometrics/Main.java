cb8ba4fd16f0c45d59cbe530afc497d1fe6f3e79
luis.amengual <luis.amengual@globant.com>
2020-11-27 18:40:56 -0300

    Agrego correcciones de log y cambio a version 1.1.11

--- src/main/java/com/biometrics/Main.java
+++ src/main/java/com/biometrics/Main.java
@@ -1,5 +1,8 @@
 package com.biometrics;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.varia.NullAppender;
 import org.neogroup.warp.WarpApplication;
 
 import java.util.Date;
@@ -12,13 +15,17 @@ import static org.neogroup.warp.Warp.getLogger;
 public class Main {
 
     public static void main(String[] args) {
-        initializeLogger();
+        initializeLogging();
         initializeOpenCV();
         WarpApplication application = new WarpApplication(args.length == 1 ? Integer.parseInt(args[0]) : 80);
+        application.addClassPath("com.biometrics");
         application.start();
     }
 
-    private static void initializeLogger() {
+    private static void initializeLogging() {
+        Logger.getRootLogger().setLevel(Level.OFF);
+        Logger.getRootLogger().removeAllAppenders();
+        Logger.getRootLogger().addAppender(new NullAppender());
         getLogger().setUseParentHandlers(false);
         ConsoleHandler handler = new ConsoleHandler();
         handler.setFormatter(new SimpleFormatter() {
