0a89cd36fbd8adfb73592a0756e13d3279a72c40
Luis Amengual <luismanuelamengual@gmail.com>
2021-11-06 20:08:12 -0300

    Subo version 1.5.0 del server

---
+++ scripts/heroku-deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "DEPLOYING TO HEROKU !!"
+heroku login
+heroku container:login
+heroku container:push web --arg JAR_FILE=target/Biometrics-1.5.0-jar-with-dependencies.jar -a=biometrics-server
+heroku container:release web -a=biometrics-server
+echo "DEPLOY TO HEROKU SUCCESS !!"
