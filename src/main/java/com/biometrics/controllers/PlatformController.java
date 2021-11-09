3461afeaf8c3c9f49a76b6155fa885d9c6ea4b05
Luis Amengual <luismanuelamengual@gmail.com>
2021-11-09 15:19:41 -0300

    Agrego controlador de plataforma

--- src/main/java/com/biometrics/controllers/PlatformController.java
+++ src/main/java/com/biometrics/controllers/PlatformController.java
@@ -13,10 +13,11 @@ import org.neogroup.warp.resources.Resources;
 
 import java.util.Collection;
 
+import static org.neogroup.warp.Warp.getRequest;
 import static org.neogroup.warp.Warp.getResponse;
 
-@ControllerComponent("clients/:sessionId")
-public class ClientsController {
+@ControllerComponent("platform/:sessionId")
+public class PlatformController {
 
     private static final String ADMINISTRATOR_SESSION_ID = "8cf3e790-6cd6-483b-9297-412b30d61328";
 
@@ -34,8 +35,9 @@ public class ClientsController {
     }
 
     @Get("liveness")
-    public Collection<DataObject> getLivenessSessions(@Param("sessionId") String sessionId, @Param(name="limit", required = false) Integer limit) {
+    public Collection<DataObject> getLivenessSessions(@Param(name="limit", required = false) Integer limit) {
         Collection<DataObject> livenessSessions = Resources.get(LivenessResource.NAME).limit(limit != null? limit : 10).orderBy(LivenessResource.Fields.ID, SortDirection.DESC).select(LivenessResource.Fields.ID, LivenessResource.Fields.CLIENT_IP, LivenessResource.Fields.CLIENT_ID, LivenessResource.Fields.STATUS, LivenessResource.Fields.DATE, LivenessResource.Fields.SUCCESS, LivenessResource.Fields.VERSION).find();
+        String sessionId = getRequest().get("sessionId");
         for (DataObject livenessSession : livenessSessions) {
             processLivenessSession(sessionId, livenessSession);
         }
@@ -43,33 +45,32 @@ public class ClientsController {
     }
 
     @Get("liveness/:id")
-    public DataObject getLivenessSession(@Param("sessionId") String sessionId, @Param("id") int livenessId) {
+    public DataObject getLivenessSession(@Param("id") int livenessId) {
         DataObject livenessSession = Resources.get(LivenessResource.NAME).select(LivenessResource.Fields.ID, LivenessResource.Fields.DATE, LivenessResource.Fields.VERSION, LivenessResource.Fields.SUCCESS, LivenessResource.Fields.STATUS, LivenessResource.Fields.CLIENT_ID, LivenessResource.Fields.CLIENT_IP).where(LivenessResource.Fields.ID, livenessId).first();
         if (livenessSession == null) {
             throw new RuntimeException("Liveness session \"" + livenessId + "\" not found !!");
         }
+        String sessionId = getRequest().get("sessionId");
         processLivenessSession(sessionId, livenessSession);
         return livenessSession;
     }
 
     @Get("liveness/:id/faceImage.jpeg")
-    public byte[] getLivenessFaceImage(@Param("id") int livenessId) {
+    public void getLivenessFaceImage(@Param("id") int livenessId) {
         DataObject livenessSession = Resources.get(LivenessResource.NAME).select(LivenessResource.Fields.FACE_IMAGE).where(LivenessResource.Fields.ID, livenessId).first();
         if (livenessSession == null) {
             throw new RuntimeException("Liveness session \"" + livenessId + "\" not found !!");
         }
-        getResponse().addHeader(Header.CONTENT_TYPE, MediaType.IMAGE_JPEG);
-        return livenessSession.get(LivenessResource.Fields.FACE_IMAGE);
+        getResponse().addHeader(Header.CONTENT_TYPE, MediaType.IMAGE_JPEG).print((byte[])livenessSession.get(LivenessResource.Fields.FACE_IMAGE));
     }
 
     @Get("liveness/:id/zoomedFaceImage.jpeg")
-    public byte[] getLivenessZoomedFaceImage(@Param("id") int livenessId) {
+    public void getLivenessZoomedFaceImage(@Param("id") int livenessId) {
         DataObject livenessSession = Resources.get(LivenessResource.NAME).select(LivenessResource.Fields.ZOOMED_FACE_IMAGE).where(LivenessResource.Fields.ID, livenessId).first();
         if (livenessSession == null) {
             throw new RuntimeException("Liveness session \"" + livenessId + "\" not found !!");
         }
-        getResponse().addHeader(Header.CONTENT_TYPE, MediaType.IMAGE_JPEG);
-        return livenessSession.get(LivenessResource.Fields.ZOOMED_FACE_IMAGE);
+        getResponse().addHeader(Header.CONTENT_TYPE, MediaType.IMAGE_JPEG).print((byte[])livenessSession.get(LivenessResource.Fields.ZOOMED_FACE_IMAGE));
     }
 
     private void processLivenessSession(String sessionId, DataObject livenessSession) {
